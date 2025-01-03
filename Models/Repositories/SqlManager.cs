c6acbd6b614c95601f5e5dfca2660862f6323847
Ycaro Gabriel da Costa Batalha <ycarobatalha1@gmail.com>
2025-01-02 21:46:07 -0300

    Tabelas do sistema de empréstimo de livros

--- Models/Repositories/SqlManager.cs
+++ Models/Repositories/SqlManager.cs
@@ -8,11 +8,11 @@ public static class SqlManager
     {
         var sql = operacaoSql switch
         {
-            TSql.LISTAR_LIVROS => "select convert(varchar(36), id) 'id', nome, autor, editora from Livros order by nome",
-            TSql.CADASTRAR_LIVRO => "insert into Livros (id, nome, autor, editora, statusLivroId) values (convert(varchar(36), @id) , @nome, @autor, @editora, @statusLivroId)",
-            TSql.PESQUISAR_LIVRO => "select convert(varchar(36), id) 'id', nome, autor, editora from Livros where id = @id",
-            TSql.EXCLUIR_LIVRO => "delete from Livros where id = @id",
-            TSql.ATUALIZAR_LIVRO => "update Livros set nome = @nome, autor = @autor, editora = @editora where id = @id",
+            TSql.LISTAR_LIVROS => "select convert(varchar(36), id) 'id', nome, autor, editora from Livro order by nome",
+            TSql.CADASTRAR_LIVRO => "insert into Livro (id, nome, autor, editora, statusLivroId) values (convert(varchar(36), @id) , @nome, @autor, @editora, @statusLivroId)",
+            TSql.PESQUISAR_LIVRO => "select convert(varchar(36), id) 'id', nome, autor, editora from Livro where id = @id",
+            TSql.EXCLUIR_LIVRO => "delete from Livro where id = @id",
+            TSql.ATUALIZAR_LIVRO => "update Livro set nome = @nome, autor = @autor, editora = @editora where id = @id",
             _ => throw new ArgumentOutOfRangeException(nameof(operacaoSql), operacaoSql, null)
         };
 
